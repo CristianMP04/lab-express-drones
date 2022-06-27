1fd304bce61ac08282d131b549ffef787937a7f5
CristianMP04 <christian.marti.pernin@gmail.com>
2022-06-27 23:13:39 +0200

    nuevo update
    nuevo update

--- routes/drones.js
+++ routes/drones.js
@@ -46,7 +46,12 @@ router.get('/drones/:id/edit', (req, res, next) => {
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+    const { id } = req.params;
+    const { name, propellers, maxSpeed} = req.body;
+ 
+  Drone.findByIdAndUpdate(id, { name, propellers, maxSpeed}, { new: true })
+    .then(updatedDrone => res.redirect(`/drones`)) // go to the details page to see the updates
+    .catch(error => next(error));
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
