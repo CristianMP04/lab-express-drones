6096414afdb020f72ce4561bd78229e5d7dd25b0
CristianMP04 <christian.marti.pernin@gmail.com>
2022-06-27 18:42:41 +0200

    update 3
    update 3

--- routes/drones.js
+++ routes/drones.js
@@ -17,14 +17,19 @@ router.get('/drones', (req, res, next) => {
 });
 
 router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  res.render('drones/create-form.hbs')
 });
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+const {name, propellers, maxSpeed} = req.body
+Drone.create({ name, propellers, maxSpeed })
+    /*.then(droneFromDB => console.log(`New drone created: ${droneFromDB.name}.` */
+    .then(() => res.redirect('/drones'))
+    .catch(error => res.redirect('/drones/create'));
 });
+
+
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
