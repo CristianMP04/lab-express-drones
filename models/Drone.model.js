e51f2362206f7878b735d7a5564b70c19fce54e8
CristianMP04 <christian.marti.pernin@gmail.com>
2022-06-27 16:43:32 +0200

    ite 01
    ite 01

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,21 @@
 // Iteration #1
+const mongoose = require("mongoose");
+
+const dronSchema = new mongoose.Schema({
+    name: {
+        type: String
+    },
+
+    propellers: {
+        type: Number
+    },
+
+    maxSpeed: {
+        type: Number
+    }
+
+});
+
+const Drone = mongoose.model("Drone", dronSchema);
+
+module.exports = Drone;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,25 @@
 // Iteration #1
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
+
+  const mongoose = require("mongoose");
+  const Drone = require("../models/Drone.model");
+
+  mongoose
+  .connect("mongodb://0.0.0.0:27017/lab-express-drones") // posible error
+  .then((response) => {
+    console.log("conectados a la Base de Datos");
+
+    return Drone.insertMany(drones);
+  })
+  .then((response) => {
+    console.log("Peliculas agregadas correctamente");
+    // hacer la desconeccion
+    mongoose.connection.close();
+  })
+  .catch((err) => {
+    console.log(err);
+  });
